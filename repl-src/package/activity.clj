5271e92cf331c0297e4b2dbf03562fd1bc23acfb
Ryo Akagi <ryoakg@gmail.com>
2016-12-07 22:09:33 +0900

    各パッケージに入っているActivityの情報 まだ内容はない

---
+++ repl-src/package/activity.clj
@@ -0,0 +1,11 @@
+(in-ns com.example.droid1.main)
+
+;;; https://developer.android.com/reference/android/content/pm/PackageManager.html のメソッド
+;; |       getActivityBanner | android.graphics.drawable.Drawable |                         [android.content.Intent] |
+;; |       getActivityBanner | android.graphics.drawable.Drawable |                  [android.content.ComponentName] |
+;; |         getActivityIcon | android.graphics.drawable.Drawable |                  [android.content.ComponentName] |
+;; |         getActivityIcon | android.graphics.drawable.Drawable |                         [android.content.Intent] |
+;; |         getActivityLogo | android.graphics.drawable.Drawable |                  [android.content.ComponentName] |
+;; |         getActivityLogo | android.graphics.drawable.Drawable |                         [android.content.Intent] |
+;; |  getPreferredActivities |                                int | [java.util.List java.util.List java.lang.String] |
+;; | getResourcesForActivity |      android.content.res.Resources |                  [android.content.ComponentName] |
